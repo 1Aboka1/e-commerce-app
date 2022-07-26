aa5d04d456f2901cf5a00efe1092c5af5ff63598
1Aboka1 <zz.abulhair.zz@gmail.com>
2022-07-26 12:16:27 +0600

    client init; tailwind init;

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,23 @@
-import Home from "./admin-pages/pages/home/Home"
-import Login from "./admin-pages/pages/login/Login"
-import List from "./admin-pages/pages/list/List"
-import Single from "./admin-pages/pages/single/Single"
-import New from "./admin-pages/pages/new/New"
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/admin">
-            <Route index element={<Home/>}></Route>
-            <Route path="login" element={<Login/>}></Route>
-            <Route path="users">
-              <Route index element={<List/>}></Route>
-              <Route path=":userId" element={<Single/>}></Route>
-              <Route path="new" element={<New/>}></Route>
-            </Route>
-            <Route path="products">
-              <Route index element={<List/>}></Route>
-              <Route path=":productID" element={<Single/>}></Route>
-              <Route path="new" element={<New/>}></Route>
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
