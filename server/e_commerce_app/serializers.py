1a58b4886334071e813e05ca052985bed9efb7fa
1Aboka1 <zz.abulhair.zz@gmail.com>
2022-08-06 21:18:12 +0600

    check 2

--- server/e_commerce_app/serializers.py
+++ server/e_commerce_app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Product
-
+import math
 BASE_URL = 'http://localhost:8000/'
 class ProductSerializer(serializers.ModelSerializer):    
     class Meta:
