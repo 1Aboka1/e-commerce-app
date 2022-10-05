d0078626851abd406f9864f985b20ae9600bbe80
1Aboka1 <zz.abulhair.zz@gmail.com>
2022-10-05 21:49:14 +0600

    sign up warnings done

--- client/src/components/SignInUp.jsx
+++ client/src/components/SignInUp.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined'
 import FormGroup from '@mui/material/FormGroup'
@@ -8,9 +10,31 @@ import Checkbox from '@mui/material/Checkbox'
 
 export const SignInUp = (props) => {
     const loginAlias = 'login'
-	const registerAlias = 'register'
+    const registerAlias = 'register'
+    const emailAlias = 'email'
+    const passwordAlias = 'password'
+    const cpasswordAlias = 'cpassword'
+
     const [windowType, setWindowType] = useState(props.type)
     const [checked, setChecked] = useState(false)
+    const [registrationInfo, setRegistrationInfo] = useState({
+    	"email": "",
+	"password": "",
+	"cpassword": "",
+    })
+    const [warnings, setWarnings] = useState({
+    	"email": false,
+	"password": false,
+	"cpassword": false,
+	"agreementChecked": false,
+    })
+    const [registrationStatus, setRegistrationStatus] = useState(null)
+
+    const handleRegistrationInputChange = (inputType) => (event) => {
+	let tempRegistrationInfo = Object.assign({}, registrationInfo)
+	tempRegistrationInfo[inputType] = event.target.value
+	setRegistrationInfo(tempRegistrationInfo)
+    }
 
     const handleWindowChange = () => {
         setWindowType(windowType === loginAlias ? registerAlias : loginAlias)
@@ -20,10 +44,32 @@ export const SignInUp = (props) => {
     	setChecked(!checked)
     }
 
-    // const handleSignUpClick = () => {
+    const handleRegister = async (event) => {
+    	event.preventDefault()
+	setWarnings({
+	    "email": registrationInfo[emailAlias].length === 0,
+	    "password": registrationInfo[passwordAlias].length === 0,
+	    "cpassword": registrationInfo[cpasswordAlias].length === 0,
+	    "agreementChecked": !checked,
+	})
+	let shouldSendForm = true
+	for(const key in warnings) {
+	    if(warnings[key] === true) { shouldSendForm = false; break }
+	}
+	
+	if(shouldSendForm === false) { return (null) }
+	axios
+	    .get(
+		'/api/user_manage/register',
+		{ params: registrationInfo },
+		{ headers: { 'Content-Type': 'application/json', } },
+	    )
+	    .then((response) => {
+	    	setRegistrationStatus(response.data.status)
+	    })
+	    .catch((error) => { console.log(error) })
+    }
 
-    // }
-    
     const renderSignInWindow = () => {
         return (
             <div className='bg-gray-100 rounded-2xl w-[75vh]'>
@@ -41,7 +87,9 @@ export const SignInUp = (props) => {
                             <VisibilityOutlinedIcon className='mx-3 cursor-pointer'/>
                         </div>
                         <p className='text-green-500 hover:text-green-600 transition ease-in-out duration-300' href="">Забыли пароль?</p>
-                        <button className='w-full h-12 font-extrabold text-white bg-green-500 rounded-2xl hover:bg-green-600 transition ease-in-out duration-300'>Войти</button>
+                        <input type="submit" value="Submit Registration">
+                            <button className='w-full h-12 font-extrabold text-white bg-green-500 rounded-2xl hover:bg-green-600 transition ease-in-out duration-300'>Войти</button>
+                        </input>
                     </form>
                 </div>
                 <div className='flex flex-row justify-center border-t border-gray-300 py-7'>
@@ -61,19 +109,23 @@ export const SignInUp = (props) => {
                     </div>
                 </div>
                 <div className='my-3'>
-                    <form className='flex flex-col items-center px-10 space-y-3'>
-                        <input className='w-full h-12 p-4 outline-none rounded-xl ring-1 ring-gray-400 focus-within:ring-1 focus-within:ring-green-400 transition ease-in-out duration-300' type="text" placeholder='E-mail' />
+                    <form onSubmit={handleRegister} className='flex flex-col items-center px-10 space-y-3'>
+                        <input type="text" value={registrationInfo[emailAlias]} onChange={handleRegistrationInputChange(emailAlias)} className='w-full h-12 p-4 outline-none rounded-xl ring-1 ring-gray-400 focus-within:ring-1 focus-within:ring-green-400 transition ease-in-out duration-300' placeholder='E-mail' />
+			{ warnings[emailAlias] ? <p className='text-red-500'>*Введите E-mail</p> : (null) }
                         <div className='flex flex-row items-center justify-between w-full bg-white rounded-xl ring-1 ring-gray-400 focus-within:ring-1 focus-within:ring-green-400 transition ease-in-out duration-300'>
-                            <input className='w-full h-12 p-4 outline-none rounded-xl' type="password" placeholder='Пароль' />
+                            <input value={registrationInfo[passwordAlias]} onChange={handleRegistrationInputChange(passwordAlias)} className='w-full h-12 p-4 outline-none rounded-xl' type="password" placeholder='Пароль' />
                             <VisibilityOutlinedIcon className='mx-3 cursor-pointer'/>
                         </div>
-                        <input className='w-full h-12 p-4 outline-none rounded-xl ring-1 ring-gray-400 focus-within:ring-1 focus-within:ring-green-400 transition ease-in-out duration-300' type="password" placeholder='Пароль' />
+			{ warnings[passwordAlias] ? <p className='text-red-500'>*Введите пароль</p> : (null) }
+                        <input value={registrationInfo[cpasswordAlias]} onChange={handleRegistrationInputChange(cpasswordAlias)} className='w-full h-12 p-4 outline-none rounded-xl ring-1 ring-gray-400 focus-within:ring-1 focus-within:ring-green-400 transition ease-in-out duration-300' type="password" placeholder='Повторите пароль' />
+			{ warnings[cpasswordAlias] ? <p className='text-red-500'>*Введите пароль повторно</p> : (null) }
 			<div>
 			    <FormGroup>
 				<FormControlLabel className='text-gray-600 text-sm' control={<Checkbox checked={checked} onChange={handleCheckboxChange} color="success"/>} label="Я согласен с условием пользования" />
 			    </FormGroup>
-			</div>
-                        <button className='w-full h-12 font-extrabold text-white bg-green-500 rounded-2xl hover:bg-green-600 transition ease-in-out duration-300'>Зарегистрироваться</button>
+			    { warnings['agreementChecked'] ? <p className='text-red-500'>*Вы должны согласиться с условиями пользования</p> : (null) }
+                        </div>
+			<input type="submit" value="Зарегистрироваться" className='w-full h-12 font-extrabold text-white bg-green-500 cursor-pointer rounded-2xl hover:bg-green-600 transition ease-in-out duration-300'/>
                     </form>
                 </div>
                 <div className='flex flex-row justify-center border-t border-gray-300 py-7'>
